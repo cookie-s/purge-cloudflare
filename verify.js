25b0ecb15bdc142a3bc4070131046bf749ef424c
cookie-s <kcz@kcz.sh>
2022-07-04 20:05:35 -0400

    add MIT License text in verify.js

--- verify.js
+++ verify.js
@@ -2,6 +2,34 @@ const timingSafeCompare = require("tsscmp");
 const crypto = require("crypto");
 
 // mainly copied from  https://github.com/slackapi/node-slack-sdk/blob/0963851e914dbc0a59d9f80305705bda2d0b9bd8/packages/interactive-messages/src/http-handler.ts#L53-L94
+
+/*** https://github.com/slackapi/node-slack-sdk/blob/0963851e914dbc0a59d9f80305705bda2d0b9bd8/LICENSE
+
+MIT License
+
+Copyright (c) 2014-2020 Slack Technologies, Inc.
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ ***/
+
 exports.verifyRequestSignature = (signingSecret, requestHeaders, body) => {
   const debug = require("debug")("verifyRequestSignature");
 
